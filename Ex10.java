7c757f3d1e4c9bc4c48dfbd9fd12c61f22fc963c
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-17 03:44:08 -0300

    Exercise 10

--- Ex10.java
+++ Ex10.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Ex10 {
     public static void run(){
-        System.out.println("Hello world!10");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please insert a value between 1 and 5: ");
+        int n = input.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("Este é o número Um");
+                break;
+            case 2:
+                System.out.println("Este é o número Dois");
+                break;
+            case 3:
+                System.out.println("Este é o número Três");
+                break;
+            case 4:
+                System.out.println("Este é o número Quatro");
+                break;
+            case 5:
+                System.out.println("Este é o número Cinco");
+                break;
+        
+            default:
+                System.out.println("Número invalido!");
+                break;
+        }   
     }
 }
