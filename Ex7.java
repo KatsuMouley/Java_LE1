346d56bde1a5e795ac29990793d8fd2cee6c7752
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-17 03:21:47 -0300

    Exercise 7

--- Ex7.java
+++ Ex7.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Ex7 {
     public static void run(){
-        System.out.println("Hello world!7");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please input a number: ");
+        double n = input.nextInt();
+        if (n > 100 && n < 200) {
+            System.out.printf("The number %.2f is between 100 and 200\n", n);
+        } else {
+            System.out.printf("The number %.2f is NOT between 100 and 200\n", n);
+        }
     }
 }
