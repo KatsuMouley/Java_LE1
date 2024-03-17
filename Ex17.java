1ae1f535fc401a3fe48d380201b91637cc6a446e
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-17 05:45:43 -0300

    Exercise 17

--- Ex17.java
+++ Ex17.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Ex17 {
     public static void run(){
-        System.out.println("Hello world!17");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please input nota1: ");
+        double n1 = input.nextDouble();
+        System.out.printf("Please input nota2: ");
+        double n2 = input.nextDouble();
+        System.out.printf("Please input nota3: ");
+        double n3 = input.nextDouble();
+        System.out.printf("Please input peso1: ");
+        double p1 = input.nextDouble();
+        System.out.printf("Please input peso2: ");
+        double p2 = input.nextDouble();
+        System.out.printf("Please input peso3: ");
+        double p3 = input.nextDouble();
+        double mediaP = ((n1*p1)+(n2*p2)+(n3*p3))/(p1+p2+p3);
+        System.out.printf("A media ponderada do aluno é a seguinte: %.2f\n", mediaP);
+                  
     }
 }
