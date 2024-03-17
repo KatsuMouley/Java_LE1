d86e178f95c1e3d01f52b51a533b236a53c08c05
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-17 05:25:14 -0300

    Exercise 16

--- Ex16.java
+++ Ex16.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Ex16 {
     public static void run(){
-        System.out.println("Hello world!16");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please input n1(Nota 1 do aluno): ");
+        double n1 = input.nextDouble();
+        System.out.printf("Please input n2(Nota 2 do aluno): ");
+        double n2 = input.nextDouble();
+        System.out.printf("Please input n3(Nota 3 do aluno): ");
+        double n3 = input.nextDouble();
+        double media = (n1 + n2 + n3) / 3;
+        System.out.printf("A média do alubo é de %.2f\n", media);
+                  
     }
 }
