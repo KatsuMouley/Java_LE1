a0696b3e5b207150fb97eb6ee681be98bbbdd830
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-17 03:18:21 -0300

    Exercise 6

--- Ex6.java
+++ Ex6.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Ex6 {
     public static void run(){
-        System.out.println("Hello world!6");
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Please input a Celsius temperature: ");
+        double C = input.nextInt();
+        double F = (9 * C + 160) / 5;
+        System.out.printf("-------------------------------------\n");
+        System.out.printf("%.2f Fahrenheit\n", F);
     }
 }
